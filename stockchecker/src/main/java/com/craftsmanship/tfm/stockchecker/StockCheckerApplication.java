b7f214183a6064d6536b0aee0fb8671e36e9c763
alb3rtobr <alb3rtobr@users.noreply.github.com>
2019-04-01 01:15:21 +0200

    stockchecker project

---
+++ stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/StockCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.craftsmanship.tfm.stockchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockCheckerApplication.class, args);
+	}
+
+}
---
+++ stockchecker/src/test/java/com/craftsmanship/tfm/stockchecker/StockCheckerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.craftsmanship.tfm.stockchecker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockCheckerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
