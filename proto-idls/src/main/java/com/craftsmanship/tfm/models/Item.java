2da6a311872cb25b4cf761f7129d5f1e3fd818e4
alb3rtobr <alb3rtobr@users.noreply.github.com>
2019-05-01 09:33:39 +0200

    Add stock attr to Item class

--- proto-idls/src/main/java/com/craftsmanship/tfm/models/Item.java
+++ proto-idls/src/main/java/com/craftsmanship/tfm/models/Item.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class Item {
     private Long id;
+    private int stock;
     private String description;
 
     private Item() {
@@ -11,6 +12,13 @@ public class Item {
 
     private Item(String description) {
         this.id = 0L;
+        this.stock = 0;
+        this.description = description;
+    }
+    
+    private Item(int stock, String description) {
+        this.id = 0L;
+        this.stock = stock;
         this.description = description;
     }
 
@@ -22,6 +30,14 @@ public class Item {
         this.id = id;
     }
 
+    public int getStock() {
+    	return this.stock;
+    }
+    
+    public void setStock(int stock) {
+    	this.stock = stock;
+    }
+    
     public String getDescription() {
         return this.description;
     }
@@ -38,18 +54,19 @@ public class Item {
             return false;
         }
         Item item = (Item) o;
-        return id == item.id && Objects.equals(description, item.description);
+        return id == item.id && stock == item.stock && Objects.equals(description, item.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, description);
+        return Objects.hash(id, stock, description);
     }
 
     @Override
     public String toString() {
         return "{" +
             " id='" + this.id + "'" +
+        	", stock='"+ this.stock + "'" +
             ", description='" + this.description + "'" +
             "}";
     }
@@ -57,15 +74,22 @@ public class Item {
     public static class Builder {
 
         private Long id;
+        private int stock;
         private String description;
 
         public Builder() {
             this.id = 0L;
+            this.stock=0;
         }
 
         public Builder withId(Long id) {
             this.id = id;
             return this;
+        }
+        
+        public Builder withStock(int stock) {
+        	this.stock=stock;
+        	return this;
         }
 
         public Builder withDescription(String description) {
@@ -76,6 +100,7 @@ public class Item {
         public Item build() {
             Item item = new Item();
             item.id = this.id;
+            item.stock = this.stock;
             item.description = this.description;
 
             return item;
