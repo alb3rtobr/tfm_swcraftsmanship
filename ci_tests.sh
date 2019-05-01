91d342bdc893f42bf0937b88d99211d548f79d4c
alb3rtobr <alb3rtobr@users.noreply.github.com>
2019-05-01 17:57:55 +0200

    Check mvn exit code

--- ci_tests.sh
+++ ci_tests.sh
@@ -4,10 +4,24 @@ dirs=('restapi' 'stockchecker')
 
 cd proto-idls
 mvn clean install
+if [ $? -ne 0 ];then
+  print "Error while running 'mvn clean install' in proto-idls project."
+  return 1
+fi
 cd ..
 
+err=0
 for dir in ${dirs[@]}; do
-  cd $dir
+  cd ${dir}
   mvn test
+  exit_code=$?
+  if [ ${exit_code} -ne 0 ];then
+    print "Error while running 'mvn test' in ${dir} project."
+    err=$((err + exit_code))
+  fi
   cd ..
 done
+
+if [ ${err} -ne 0 ];then
+  return 1
+fi
