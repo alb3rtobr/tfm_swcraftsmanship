e7fd6bb3f7453521f0e4b1cf64accc47f05fc16d
carlostorres <ctorres14@gmail.com>
2019-05-04 09:32:12 +0200

    Item model class as Entity to persist.

---
+++ dal/src/main/java/com/craftsmanship/tfm/dal/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.craftsmanship.tfm.dal.repository;
+
+import com.craftsmanship.tfm.dal.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+	
+}
