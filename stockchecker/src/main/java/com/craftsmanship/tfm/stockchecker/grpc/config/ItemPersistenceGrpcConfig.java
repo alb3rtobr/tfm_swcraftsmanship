9853a489169e464255402d711456df766b15c0cc
alb3rtobr <alb3rtobr@users.noreply.github.com>
2019-05-14 23:19:27 +0200

    Missing from previous commits

--- stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/grpc/config/ItemPersistenceGrpcConfig.java
+++ stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/grpc/config/ItemPersistenceGrpcConfig.java
@@ -1,12 +1,12 @@
 package com.craftsmanship.tfm.stockchecker.grpc.config;
 
-import com.craftsmanship.tfm.stockchecker.grpc.ItemsPersistence;
-import com.craftsmanship.tfm.stockchecker.grpc.ItemsPersistenceGrpc;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.craftsmanship.tfm.persistence.ItemPersistence;
+import com.craftsmanship.tfm.persistence.ItemPersistenceGrpc;
 
 @Configuration
 @Profile("!dev")
@@ -19,7 +19,7 @@ public class ItemPersistenceGrpcConfig {
   private int serverPort;
 
   @Bean
-  public ItemsPersistence persistenceHandler() {
-    return new ItemsPersistenceGrpc(serverHost, serverPort);
+  public ItemPersistence persistenceHandler() {
+    return new ItemPersistenceGrpc(serverHost, serverPort);
   }
 }
