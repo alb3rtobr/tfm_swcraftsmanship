62d8b2cd5ec5256b4b409ab4088df19302f8c4d3
kikogolk <kikogolk@users.noreply.github.com>
2019-05-15 18:32:18 +0200

    First version of build script

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+BASE_DIR=`pwd`
+
+SERVICES="restapi stockchecker dal"
+
+function build_protos() {
+    echo "---------------------------------------------"
+    echo " BUILDING proto-idls"
+    echo "---------------------------------------------"
+    cd ${BASE_DIR}/proto-idls
+    mvn clean install || exit $?
+}
+
+function build_service() {
+    service=$1
+    echo "---------------------------------------------"
+    echo " BUILDING $service"
+    echo "---------------------------------------------"
+    cd ${BASE_DIR}/${service}
+    mvn clean package || exit $?
+}
+
+function build_docker_image() {
+    service=$1
+    echo "---------------------------------------------"
+    echo " CREATING DOCKER IMAGE: almacar_${service}"
+    echo "---------------------------------------------"
+    cd ${BASE_DIR}/${service}
+    docker build --tag=almacar_${service}:0.1 --rm=true . || exit $?
+}
+
+function delete_docker_images() {
+    service=$1
+    echo "---------------------------------------------"
+    echo " DELETING DOCKER IMAGE: almacar_${service}"
+    echo "---------------------------------------------"
+    cd ${BASE_DIR}/${service}
+    docker rmi almacar_${service}:0.1 || exit $?
+}
+
+build_protos
+
+for service in $SERVICES; do
+    build_service $service
+    delete_docker_image $service
+    build_docker_image $service
+done
+
