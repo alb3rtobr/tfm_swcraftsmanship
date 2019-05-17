6e507b7f0f6df347559d5bfe3967325cc674b140
kikogolk <kikogolk@users.noreply.github.com>
2019-05-18 01:10:21 +0200

    RESTAPI: Now using ConfigMap as configuration

--- restapi/src/main/java/com/craftsmanship/tfm/restapi/kafka/config/KafkaConfig.java
+++ restapi/src/main/java/com/craftsmanship/tfm/restapi/kafka/config/KafkaConfig.java
@@ -3,21 +3,24 @@ package com.craftsmanship.tfm.restapi.kafka.config;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.craftsmanship.tfm.restapi.kafka.service.ItemOperationService;
 import com.craftsmanship.tfm.models.ItemOperation;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
-public class KafkaProducerConfig {
+@ConfigurationProperties(prefix = "kafka")
+public class KafkaConfig {
 
     @Value(value = "${kafka.bootstrap-servers}")
     private String bootstrapServers;
@@ -34,5 +37,12 @@ public class KafkaProducerConfig {
     @Bean
     public KafkaTemplate<String, ItemOperation> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
     }
 }
--- restapi/src/main/java/com/craftsmanship/tfm/restapi/persistence/config/PersistenceConfig.java
+++ restapi/src/main/java/com/craftsmanship/tfm/restapi/persistence/config/PersistenceConfig.java
@@ -1,16 +1,21 @@
 package com.craftsmanship.tfm.restapi.persistence.config;
 
-import com.craftsmanship.tfm.persistence.ItemPersistence;
 import com.craftsmanship.tfm.persistence.ItemPersistenceGrpc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@ConfigurationProperties(prefix = "dal")
 @Profile("!dev")
 public class PersistenceConfig {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(PersistenceConfig.class);
 
   @Value(value = "${dal.host}")
   private String serverHost;
@@ -19,7 +24,7 @@ public class PersistenceConfig {
   private int serverPort;
 
   @Bean
-  public ItemPersistence persistenceHandler() {
+  public ItemPersistenceGrpc persistenceHandler() {
     return new ItemPersistenceGrpc(serverHost, serverPort);
   }
 }
