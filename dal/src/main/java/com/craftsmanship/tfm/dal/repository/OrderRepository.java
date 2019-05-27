48cdccad3ae625ad058684bbb3642230191a8ec0
carlostorres <ctorres14@gmail.com>
2019-05-27 17:23:18 +0200

    #12: OrderDAO created.

--- dal/src/main/java/com/craftsmanship/tfm/dal/repository/OrderRepository.java
+++ dal/src/main/java/com/craftsmanship/tfm/dal/repository/OrderRepository.java
@@ -5,6 +5,6 @@ import com.craftsmanship.tfm.dal.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-	
+
 }
 
