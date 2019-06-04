9533aa8299245de378ff7d6501ffbfa30851dcb0
kikogolk <kikogolk@users.noreply.github.com>
2019-06-05 00:49:38 +0200

    Fixed DAL UT after introducing Custom Metrics

--- dal/src/test/java/com/craftsmanship/tfm/dal/model/ItemDAOTest.java
+++ dal/src/test/java/com/craftsmanship/tfm/dal/model/ItemDAOTest.java
@@ -18,11 +18,15 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.CompositeMeterRegistryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@Import({MetricsAutoConfiguration.class, CompositeMeterRegistryAutoConfiguration.class})
 public class ItemDAOTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ItemDAOTest.class);
