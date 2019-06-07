2d99ada921bd08807014d9f7e1f7c98c7f79b5cb
carlostorres <ctorres14@gmail.com>
2019-06-07 21:49:41 +0200

    #111: Stockchecker metrics.

--- stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/rest/PurchaseOrderClient.java
+++ stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/rest/PurchaseOrderClient.java
@@ -6,42 +6,54 @@ import org.springframework.web.client.RestTemplate;
 
 import com.craftsmanship.tfm.models.Item;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
 public class PurchaseOrderClient implements RestClient {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PurchaseOrderClient.class);
-	
-	private final String uri;
-	
-	private int stockThreshold;
-	
-	public PurchaseOrderClient(String restHost, int restPort, String restEndPoint, int stockThreshold) {
-		
-		this.uri = "http://"+restHost+":"+restPort+"/"+restEndPoint;
-		this.restTemplate = new RestTemplate();
-		this.stockThreshold=stockThreshold;
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(PurchaseOrderClient.class);
 
-	private RestTemplate restTemplate;
-	
-	public RestTemplate getRestTemplate() {
-		return this.restTemplate;
-	}
-	
-	@Override
-	public PurchaseOrder sendPurchaseOrder(Item item, int currentStock) {
-		if (currentStock<this.stockThreshold) {
-			PurchaseOrder newOrder = new PurchaseOrder(item);
-			LOGGER.info("Items below threshold ( "+currentStock+"<"+this.stockThreshold+" ), contacting REST API.");
-			//PurchaseOrder result=restTemplate.postForObject( uri, newOrder, PurchaseOrder.class);
-			// By the moment, only a log is added.
-			// This function could be impacted by the new model.
-		    LOGGER.info("sendPurchaseOrder sent order for item ["+item.toString()+"]");
-		    return newOrder;
-			
-		}else {
-			LOGGER.info("Items above threshold ( "+currentStock+">="+this.stockThreshold+" ), NOT contacting REST API.");
-			return null;
-		}		
-	}
-	
+    private final String uri;
+
+    private int stockThreshold;
+
+    private final Counter itemOutOfStockCounter;
+
+    public PurchaseOrderClient(String restHost, int restPort, String restEndPoint, int stockThreshold, MeterRegistry meterRegistry) {
+
+        this.uri = "http://"+restHost+":"+restPort+"/"+restEndPoint;
+        this.restTemplate = new RestTemplate();
+        this.stockThreshold=stockThreshold;
+        
+        this.itemOutOfStockCounter = Counter
+                .builder("item_out_of_stock")
+                .description("Number of purchases sent due to item out of stock")
+                .register(meterRegistry);
+    }
+
+    private RestTemplate restTemplate;
+
+    public RestTemplate getRestTemplate() {
+        return this.restTemplate;
+    }
+
+    @Override
+    public PurchaseOrder sendPurchaseOrder(Item item, int currentStock) {
+        if (currentStock<this.stockThreshold) {
+            PurchaseOrder newOrder = new PurchaseOrder(item);
+            LOGGER.info("Items below threshold ( "+currentStock+"<"+this.stockThreshold+" ), contacting REST API.");
+            //PurchaseOrder result=restTemplate.postForObject( uri, newOrder, PurchaseOrder.class);
+            // By the moment, only a log is added.
+            // This function could be impacted by the new model.
+            LOGGER.info("sendPurchaseOrder sent order for item ["+item.toString()+"]");
+
+            itemOutOfStockCounter.increment();
+            return newOrder;
+
+        }else {
+            LOGGER.info("Items above threshold ( "+currentStock+">="+this.stockThreshold+" ), NOT contacting REST API.");
+            return null;
+        }		
+    }
+
 }
--- stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/rest/config/PurchaseOrderClientConfig.java
+++ stockchecker/src/main/java/com/craftsmanship/tfm/stockchecker/rest/config/PurchaseOrderClientConfig.java
@@ -2,41 +2,46 @@ package com.craftsmanship.tfm.stockchecker.rest.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.craftsmanship.tfm.stockchecker.rest.PurchaseOrderClient;
 import com.craftsmanship.tfm.stockchecker.rest.RestClient;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @Configuration
 @EnableAutoConfiguration
 @ConfigurationProperties(prefix = "stockchecker")
 public class PurchaseOrderClientConfig {
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(PurchaseOrderClientConfig.class);
-	
-	@Value(value = "${stockchecker.threshold}")
-	private int MIN_STOCK_THRESHOLD;
-	
-	@Value(value = "${stockchecker.rest.host}")
-	private String restHost;
 
-	@Value(value = "${stockchecker.rest.port}")
-	private int restPort;
-	
-	@Value(value = "${stockchecker.rest.endpoint}")
-	private String restEndPoint;
-	
-	@Bean
-	public RestClient restClient() {
-		return new PurchaseOrderClient(restHost,restPort,restEndPoint,MIN_STOCK_THRESHOLD);
-	}
-	
-	public int getThreshold() {
-		return this.MIN_STOCK_THRESHOLD;
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(PurchaseOrderClientConfig.class);
+
+    @Value(value = "${stockchecker.threshold}")
+    private int MIN_STOCK_THRESHOLD;
+
+    @Value(value = "${stockchecker.rest.host}")
+    private String restHost;
+
+    @Value(value = "${stockchecker.rest.port}")
+    private int restPort;
+
+    @Value(value = "${stockchecker.rest.endpoint}")
+    private String restEndPoint;
+
+    @Autowired
+    MeterRegistry meterRegistry;
+
+    @Bean
+    public RestClient restClient() {
+        return new PurchaseOrderClient(restHost,restPort,restEndPoint,MIN_STOCK_THRESHOLD, meterRegistry);
+    }
+
+    public int getThreshold() {
+        return this.MIN_STOCK_THRESHOLD;
+    }
 }
